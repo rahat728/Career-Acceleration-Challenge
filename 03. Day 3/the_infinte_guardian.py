65b675a74de6a92bc27e7388325cd71ad7783fb3
Asifuzzaman Rahat <51532842+rahat728@users.noreply.github.com>
2025-12-18 08:20:09 +0600

    Delete 03. Day 3/the_infinte_guardian.py

--- 03. Day 3/the_infinte_guardian.py
+++
@@ -1,7 +0,0 @@
-while True:
-    pwd = input("Enter password: ")
-    if pwd == "stop":
-        print("Access granted. Loop terminated.")
-        break
-    else:
-        print("Wrong password. Try again.")
